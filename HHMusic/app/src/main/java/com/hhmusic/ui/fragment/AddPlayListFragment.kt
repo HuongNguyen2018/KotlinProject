24faf3c25d5a15394b299be2f6814614713feb16
hungngo0309 <39835798+hungngo0309@users.noreply.github.com>
2019-08-05 17:37:00 -0700

    a

--- HHMusic/app/src/main/java/com/hhmusic/ui/fragment/AddPlayListFragment.kt
+++ HHMusic/app/src/main/java/com/hhmusic/ui/fragment/AddPlayListFragment.kt
@@ -12,13 +12,13 @@ import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.RecyclerView
-import com.hhmusic.R
-import com.hhmusic.ui.activity.MainActivity
 import com.hhmusic.ui.activity.PlayerActivity
-import com.hhmusic.ui.adapters.SongListAdapter
 import com.hhmusic.utilities.InjectorUtils
 import com.hhmusic.viewmodels.PlayListViewModel
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.widget.EditText
+
 
 class AddPlayListFragment (private val myActivity: PlayerActivity) : DialogFragment() {
 
@@ -28,13 +28,13 @@ class AddPlayListFragment (private val myActivity: PlayerActivity) : DialogFragm
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setStyle(DialogFragment.STYLE_NORMAL, R.style.FullScreenDialogStyle);
+        setStyle(DialogFragment.STYLE_NORMAL, com.hhmusic.R.style.FullScreenDialogStyleAddToPlaylist);
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var viewRoot = inflater.inflate(R.layout.dialog_fragment_add_playlist, container, false)
+        var viewRoot = inflater.inflate(com.hhmusic.R.layout.dialog_fragment_add_playlist, container, false)
 
-        var toolbar = viewRoot.findViewById<Toolbar>(R.id.toolbar)
+        var toolbar = viewRoot.findViewById<Toolbar>(com.hhmusic.R.id.toolbar)
         toolbar.setTitle("Add to PlayList")
         toolbar.setNavigationIcon(android.R.drawable.ic_menu_close_clear_cancel)
         toolbar.setNavigationOnClickListener(View.OnClickListener {
@@ -43,22 +43,20 @@ class AddPlayListFragment (private val myActivity: PlayerActivity) : DialogFragm
 
         var values : List<String> = listOf()
         values += "Create New PlayList"
-        values += "PlayList 1"
-        values += "PlayList 2"
 
         var adapter : ArrayAdapter<String> = ArrayAdapter<String>(
-                        activity!!,
-                        android.R.layout.simple_list_item_1,
+                        myActivity!!,
+                        com.hhmusic.R.layout.simple_list_item_2,
+                        //android.R.layout.simple_list_item_1,
                         android.R.id.text1,
                         values
                         )
 
         // Assign adapter to ListView
-        listView = viewRoot.findViewById(R.id.list)
+        listView = viewRoot.findViewById(com.hhmusic.R.id.list)
         listView?.setAdapter(adapter)
 
 
-/*
         val factory = InjectorUtils.provideViewModelFactory(myActivity)
         viewModel = ViewModelProviders.of(myActivity, factory).get(PlayListViewModel::class.java)
         viewModel.getPlayLists().observe(myActivity, Observer {
@@ -67,21 +65,49 @@ class AddPlayListFragment (private val myActivity: PlayerActivity) : DialogFragm
                     values += it[i].name
                 }
 
+                adapter.clear()
                 adapter.addAll(values)
                 adapter.notifyDataSetChanged()
             }
         })
-*/
+
 
 
         listView?.setOnItemClickListener(AdapterView.OnItemClickListener { parent, view, position, id ->
             // ListView Clicked item value
             val itemValue = listView?.getItemAtPosition(position) as String
             if (position == 0) {
-                Toast.makeText(activity, "User chose create new PlayList", Toast.LENGTH_SHORT).show()
+                //Toast.makeText(myActivity, "User chose create new PlayList", Toast.LENGTH_SHORT).show()
+
+                // get prompts.xml view
+                val li = LayoutInflater.from(context)
+                val promptsView = li.inflate(com.hhmusic.R.layout.prompts, null)
+
+                val alertDialogBuilder = AlertDialog.Builder(myActivity)
+
+                // set prompts.xml to alertdialog builder
+                alertDialogBuilder.setView(promptsView);
+
+                val userInput = promptsView.findViewById(com.hhmusic.R.id.editTextDialogUserInput) as EditText
+
+				// set dialog message
+				alertDialogBuilder
+                    .setCancelable(false)
+                    .setPositiveButton("Create New", DialogInterface.OnClickListener() { dialog, which ->
+                        Toast.makeText(myActivity, userInput.getText(), Toast.LENGTH_SHORT).show()
+                    })
+                    .setNegativeButton("Cancel", DialogInterface.OnClickListener() { dialog, which ->
+                        dialog.cancel()
+                    })
+
+                // create alert dialog
+                val alertDialog = alertDialogBuilder.create()
+
+                // show it
+                alertDialog.show()
 
             } else {
-                Toast.makeText(activity, "User chose " + values[position], Toast.LENGTH_SHORT).show()
+                Toast.makeText(myActivity, "User chose " + values[position], Toast.LENGTH_SHORT).show()
 
             }
         })
